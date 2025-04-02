af17086c0a3c5f92fe7518149c53f07e5ef356b6
Anand Gupta <anand.gupta@angloamerican.com>
2025-04-02 21:16:19 +0530

    RabbitMQ Consumer for new User Registration Notification

---
+++ TGA.ECommerceApp.Notification.Domain/Events/UserRegistrationEvent.cs
@@ -0,0 +1,13 @@
+﻿using TGA.ECommerceApp.Domain.Core.Events;
+
+namespace TGA.ECommerceApp.Notification.Domain.Events
+{
+    public class UserRegistrationEvent : Event
+    {
+        public string Email { get; protected set; }
+        public UserRegistrationEvent(string email)
+        {
+            Email = email;
+        }
+    }
+}
