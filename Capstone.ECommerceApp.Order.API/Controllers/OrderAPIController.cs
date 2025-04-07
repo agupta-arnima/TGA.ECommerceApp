a90cfd0937cf0837682246b6c0da0257dcfc73cd
Sachin Kumar <sachin.kumar@angloamerican.com>
2025-04-07 20:29:19 +0530

    Commit Changes

--- Capstone.ECommerceApp.Order.API/Controllers/OrderAPIController.cs
+++ Capstone.ECommerceApp.Order.API/Controllers/OrderAPIController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Capstone.ECommerceApp.Order.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/order")]
     [ApiController]
     public class OrderAPIController : ControllerBase
     {
