584b60e9f5a247b5377492019af54fa1ab07ed4e
Anand Gupta <anand.gupta@angloamerican.com>
2025-05-19 23:36:52 +0530

    Azure Service Bus Implementation

---
+++ Capstone.ECommerceApp.Infra.Bus/AzureServiceBus/ServiceBus.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Messaging.ServiceBus;
+using Capstone.ECommerceApp.Domain.Core.Bus;
+using Capstone.ECommerceApp.Domain.Core.Commands;
+using Capstone.ECommerceApp.Domain.Core.Events;
+using System.Text;
+
+namespace Capstone.ECommerceApp.Infra.Bus.MessageBus
+{
+    public sealed class ServiceBus : IEventBus
+    {
+        private readonly string _messageBusConnectionString;
+
+        public ServiceBus(string messageBusConnectionString)
+        {
+            _messageBusConnectionString = messageBusConnectionString;
+        }
+
+        public async Task PublishMessageAsync<T>(T @event, string topic_queue_name, string token = "") where T : Event
+        {
+            await using var client = new ServiceBusClient(_messageBusConnectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_name);
+
+            var jsonMessage = System.Text.Json.JsonSerializer.Serialize(@event);
+
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
+
+        public Task SendCommandAsync<T>(T command) where T : Command
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Capstone.ECommerceApp.Order.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Capstone.ECommerceApp.Domain.Core.Bus;
+using Capstone.ECommerceApp.Order.API.Messaging;
+
+namespace Capstone.ECommerceApp.Order.API.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        private static IMessageConsumer ServiceBusConsumer { get; set; }
+        public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
+        {
+            ServiceBusConsumer = app.ApplicationServices.GetService<ServiceBusConsumer>(); //Need to register the service in Dependency Injection
+            var hostApplicationLifetime = app.ApplicationServices.GetService<IHostApplicationLifetime>(); //Application lifetime is needed to start and stop the service
+            //hostApplicationLifetime.ApplicationStarted.Register(() => ServiceBusConsumer.Start());
+            //hostApplicationLifetime.ApplicationStopped.Register(() => ServiceBusConsumer.Stop());
+            hostApplicationLifetime.ApplicationStarted.Register(OnStart);
+            hostApplicationLifetime.ApplicationStopped.Register(OnStop);
+            return app;  //Don't want to hold up the pipeline
+        }
+        private static void OnStart()
+        {
+            //TBD
+            //ServiceBusConsumer.StartConsuming();
+        }
+        private static void OnStop()
+        {
+            ServiceBusConsumer.Stop();
+        }
+    }
+}
